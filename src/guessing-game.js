87c67898f7621670d6d1d2eb119a5d1ebdf7630a
Volodymyr Vasylkivskyi <vasilksvskij@gmail.com>
2023-02-22 18:33:17 +0200

    feat: add js file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.min = 0
+    this.max = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return this.number = Math.round((this.min + this.max) / 2)
     }
 
     lower() {
-
+        this.max = this.number
     }
 
     greater() {
-
+        this.min = this.number
     }
 }
 
